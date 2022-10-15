9398bebe5c0a8c9b4b883f6448a1bcd2f75fa3ea
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-15 17:46:17 +0900

    w

--- backend/test/database/user.js
+++ backend/test/database/user.js
@@ -3,91 +3,184 @@ const { sequelize, User } = require("../../db/models/index")
 const { consoleLogJson } = require("../../library/index")
 
 async function start_user() {
-    console.log("start user")
-    // create_users()
-    read_users()
+    console.log("start_user")
+    // create_user()
+    // read_users()
     // read_users2()
     // read_user()
     // update_user()
-    // destroy_user()
+    destroy_user()
     // search_users()
+}
+async function create_user() {
+    console.log("start create_user")
+
+    try {
+        const inputData = {
+            name: "abc",
+            email: "5abc@abc.com",
+            password: "passwordpassword",
+            role: "abc",
+        }
+
+        const user = await User.create(inputData)
+        // console.log("user", JSON.stringify(user, null, 2))
+
+        const msg = "Successfully created User"
+        const data = user.dataValues
+
+        console.log({ msg, data })
+    } catch (error) {
+        console.log({ error })
+    }
 }
 
 async function read_users() {
-    const users = await User.findAll({ include: 'posts' })
+    console.log("start read_users")
 
-    // console.log("users", JSON.stringify(users, null, 2))
-    consoleLogJson(users)
+    try {
+        const users = await User.findAll({ include: 'posts' })
+        // console.log("users", JSON.stringify(users, null, 2))
+
+        const data = users
+        const msg = users.length === 0 ? "Successfully read Users but empty" : "Successfully read Users"
+
+        console.log({ msg, data })
+
+    } catch (error) {
+        console.log({ error })
+    }
+
 }
 
 async function read_users2() {
-    const [results, metadata] = await sequelize.query("SELECT * FROM users");
-    console.log("users metadata", JSON.stringify(metadata, null, 2))
+    console.log("start read_users2")
+
+    try {
+        const [results, metadata] = await sequelize.query("SELECT * FROM users");
+        // console.log("users metadata", JSON.stringify(metadata, null, 2))
+        const msg = "Successfully read User"
+        const data = metadata
+
+        console.log({ msg, data })
+    } catch (error) {
+        console.log({ error })
+    }
 }
 
 async function read_user() {
-    const uuid = '35cf1b89-56d3-433c-9f43-4198eb3725de'
-    const users = await User.findOne({ where: { uuid }, include: 'posts' })
-    console.log("users", JSON.stringify(users, null, 2))
-}
+    console.log("start read_user")
 
-async function create_users() {
-    const inputData = {
-        name: "abc",
-        email: "abc@abc.com",
-        role: "abc",
+    const id = 2
+    try {
+        const user = await User.findOne({ where: { id }, include: 'posts' })
+        // console.log("JSON.stringify(user, null, 2)", JSON.stringify(user, null, 2))
+
+        console.log({ user })
+        if (user === null) {
+            const msg = "Successfully read User but empty"
+            const data = undefined
+
+            console.log({ msg, data })
+        } else {
+            const msg = "Successfully read User"
+            const data = user.dataValues
+
+            console.log({ msg, data })
+        }
+    } catch (error) {
+        console.log({ error })
     }
-    const user = await User.create(inputData)
-    console.log("user", JSON.stringify(user, null, 2))
 }
 
 async function update_user() {
-    const uuid = '35cf1b89-56d3-433c-9f43-4198eb3725de'
+    console.log("start update_user")
+    const id = 3
     const inputData = {
-        name: "abc",
-        email: "abc@abc.com",
-        role: "abc",
+        name: "updated name",
+        role: "admin",
     }
-    const user = await User.findOne({ where: { uuid } })
 
-    user.name = inputData.name
-    user.email = inputData.email
-    user.role = inputData.role
+    let foundUser
+    try {
+        foundUser = await User.findByPk(id);
 
-    await user.save()
-    console.log("user", JSON.stringify(user, null, 2))
+        console.log({ foundUser })
+    } catch (error) {
+        console.log({ error })
+    }
+
+    let user
+    try {
+        user = await User.update(inputData, {
+            where: {
+                id
+            }
+        });
+    } catch (error) {
+        console.log({ error })
+    }
+
+    foundUser.name = inputData.name
+    foundUser.role = inputData.role
+
+    const msg = "Successfully updated User"
+    const data = foundUser.dataValues
+
+    console.log({ msg, data })
 }
 
 async function destroy_user() {
-    const uuid = '35cf1b89-56d3-433c-9f43-4198eb3725de'
-    await User.destroy({
-        where: {
-            uuid
-        }
-    });
-    console.log("delete user")
+    console.log("start destroy_user")
+
+    const id = 2
+
+    try {
+        await User.destroy({
+            where: {
+                id
+            }
+        });
+    } catch (error) {
+        console.log({ error })
+    }
+
+    const msg = "Successfully deleted User"
+    const data = { id }
+
+    console.log({ msg, data })
+
 }
 
 async function search_users() {
-    const keyword = "Doe"
-    const users = await User.findAll({
-        where: {
-            [Op.or]: [
-                {
-                    name: {
-                        [Op.like]: '%' + keyword + '%'
-                    }
-                },
-                {
-                    email: {
-                        [Op.like]: '%' + keyword + '%'
-                    }
-                }
-            ]
-        }
-    })
+    console.log("start search_users")
 
-    consoleLogJson(users)
+    const keyword = "Doe"
+    try {
+        const users = await User.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        name: {
+                            [Op.like]: '%' + keyword + '%'
+                        }
+                    },
+                    {
+                        email: {
+                            [Op.like]: '%' + keyword + '%'
+                        }
+                    }
+                ]
+            }
+        })
+
+        const data = users
+        const msg = users.length === 0 ? "Successfully searched Users but empty" : "Successfully searched Users"
+
+        console.log({ msg, data })
+    } catch (error) {
+        console.log({ error })
+    }
 }
 
 module.exports = {
