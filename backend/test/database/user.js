554e27f18bbdf7094357fc9042cb42c7eee7697a
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-15 20:09:31 +0900

    JSON加工

--- backend/test/database/user.js
+++ backend/test/database/user.js
@@ -3,38 +3,43 @@ const { sequelize, User } = require("../../db/models/index")
 
 async function start_user() {
     console.log("start_user")
+
     // create_user()
     // read_users()
-    // read_users2()
+    read_users2()
+    // read_users3()
     // read_user()
-    // update_user()
-    destroy_user()
     // search_users()
+    // update_user()
+    // destroy_user()
+    // truncate_users()
+
 }
-async function create_user() {
+
+const create_user = async () => {
     console.log("start create_user")
 
-    try {
-        const inputData = {
-            name: "abc",
-            email: "5abc@abc.com",
-            password: "passwordpassword",
-            role: "abc",
-        }
+    const inputData = {
+        name: "abc",
+        email: "abc@abc.com",
+        password: "unhashed_password",
+        role: "normal",
+    }
 
+    try {
         const user = await User.create(inputData)
         // console.log("user", JSON.stringify(user, null, 2))
 
         const msg = "Successfully created User"
-        const data = user.dataValues
+        const data = user
 
-        console.log({ msg, data })
+        console.log({ isSuccess: true, msg, data })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
     }
 }
 
-async function read_users() {
+const read_users = async () => {
     console.log("start read_users")
 
     try {
@@ -42,16 +47,46 @@ async function read_users() {
         // console.log("users", JSON.stringify(users, null, 2))
 
         const data = users
-        const msg = users.length === 0 ? "Successfully read Users but empty" : "Successfully read Users"
+        const msg = users.length !== 0 ? "Successfully read Users" : "Successfully read Users but empty"
 
-        console.log({ msg, data })
-
+        console.log({ isSuccess: true, msg, data })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
     }
 }
 
-async function read_users2() {
+const read_users2 = async () => {
+    console.log("start read_users2")
+
+    try {
+        const users = await User.findAll({ include: 'posts' })
+        // console.log("users", JSON.stringify(users, null, 2))
+
+        let arrangedResData = [];
+        users.forEach(row => {
+            arrangedResData.push(
+                {
+                    "id": row.id,
+                    "name": row.name,
+                    "email": row.email,
+                    "password": row.password,
+                    "role": row.role,
+                    "avator": row.avator ? row.avator : "https://www.publicdomainpictures.net/pictures/300000/nahled/empty-white-room.jpg"
+                }
+            );
+        });
+        // console.log("arrangedResData", JSON.stringify(arrangedResData, null, 2))
+
+        const data = arrangedResData
+        const msg = users.length !== 0 ? "Successfully read Users" : "Successfully read Users but empty"
+
+        console.log({ isSuccess: true, msg, data })
+    } catch (error) {
+        console.log({ isSuccess: false, error })
+    }
+}
+
+const read_users3 = async () => {
     console.log("start read_users2")
 
     try {
@@ -60,100 +95,102 @@ async function read_users2() {
         const msg = "Successfully read User"
         const data = metadata
 
-        console.log({ msg, data })
+        console.log({ isSuccess: true, msg, data })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
     }
 }
 
-async function read_user() {
+const read_user = async () => {
     console.log("start read_user")
 
-    const id = 2
+    const id = 1
 
     try {
         const user = await User.findOne({ where: { id }, include: 'posts' })
         // console.log("JSON.stringify(user, null, 2)", JSON.stringify(user, null, 2))
 
-        console.log({ user })
-        if (user === null) {
-            const msg = "Successfully read User but empty"
-            const data = undefined
+        const msg = user ? "Successfully searched Users" : "Successfully searched Users but empty"
+        const data = user
 
-            console.log({ msg, data })
-        } else {
-            const msg = "Successfully read User"
-            const data = user.dataValues
-
-            console.log({ msg, data })
-        }
+        console.log({ isSuccess: true, msg, data })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
     }
 }
 
-async function update_user() {
+const update_user = async () => {
     console.log("start update_user")
 
-    const id = 3
+    const id = 2
 
     const inputData = {
         name: "updated name",
         role: "admin",
     }
 
-    let foundUser
+    let foundUserWithId
     try {
-        foundUser = await User.findByPk(id);
+        foundUserWithId = await User.findByPk(id);
 
-        console.log({ foundUser })
+        // console.log({ foundUserWithId })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
     }
 
-    let user
+    if (!foundUserWithId) {
+        throw new Error('user not found');
+    }
+
     try {
-        user = await User.update(inputData, {
-            where: {
-                id
-            }
+        await User.update(inputData, {
+            where: { id }
         });
+
+        foundUserWithId.name = inputData.name
+        foundUserWithId.role = inputData.role
+
+        const msg = "Successfully updated User"
+        const data = foundUserWithId
+
+        console.log({ isSuccess: true, msg, data })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
     }
-
-    foundUser.name = inputData.name
-    foundUser.role = inputData.role
-
-    const msg = "Successfully updated User"
-    const data = foundUser.dataValues
-
-    console.log({ msg, data })
 }
 
-async function destroy_user() {
+const destroy_user = async () => {
     console.log("start destroy_user")
 
     const id = 2
 
+    let foundUserWithId
     try {
-        await User.destroy({
-            where: {
-                id
-            }
-        });
+        foundUserWithId = await User.findByPk(id);
+
+        // console.log({ foundUserWithId })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
     }
 
-    const msg = "Successfully deleted User"
-    const data = { id }
+    if (!foundUserWithId) {
+        throw new Error('user not found');
+    }
 
-    console.log({ msg, data })
+    try {
+        await User.destroy({
+            where: { id }
+        });
+        const msg = "Successfully deleted User"
+        const data = foundUserWithId
 
+        console.log({ isSuccess: true, msg, data })
+    } catch (error) {
+        console.log({ isSuccess: false, error })
+    }
 }
 
-async function search_users() {
+const search_users = async () => {
     console.log("start search_users")
 
     const keyword = "Doe"
@@ -178,9 +215,26 @@ async function search_users() {
         const data = users
         const msg = users.length === 0 ? "Successfully searched Users but empty" : "Successfully searched Users"
 
-        console.log({ msg, data })
+        console.log({ isSuccess: true, msg, data })
     } catch (error) {
-        console.log({ error })
+        console.log({ isSuccess: false, error })
+    }
+}
+
+const truncate_users = async () => {
+    console.log("start truncate_users")
+
+    try {
+        await User.destroy({
+            truncate: true
+        });
+
+        const data = []
+        const msg = "Successfully truncate Users but empty"
+
+        console.log({ isSuccess: true, msg, data })
+    } catch (error) {
+        console.log({ isSuccess: false, error })
     }
 }
 
