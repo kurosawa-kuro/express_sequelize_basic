c10d5cebc2cd52baa6db403005ecbb093e89a18d
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-20 19:32:24 +0900

    sample追加

--- backend/test/db/index.js
+++ backend/test/db/index.js
@@ -1,11 +1,12 @@
 const { startUser } = require("./userDatabase.js")
 const { startPost } = require("./postDatabase.js")
+const { startSample } = require("./sample.js")
 
 async function start() {
     console.log("start database index")
-    startUser()
+    // startUser()
     // startPost()
-    // start_book()
+    startSample()
     // start_posts()
     // start_auth()
 }
